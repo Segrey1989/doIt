cba9eabd73486aac946946e7ef8c1853a2d10394
Siarhei Kvachonak <segrey1989@tut.by>
2016-09-13 15:36:08 +0300

    Initial Commit

---
+++ doIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  doIt
+//
+//  Created by Siarhei Kvachonak on 13.09.16.
+//  Copyright © 2016 Siarhei Kvachonak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
